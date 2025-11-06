3a7d8874504ecc6600bf70ba4f67eced4bd32181
taheraljabi3 <451030893@stu.iu.edu.sa>
2025-11-06 21:06:15 +0300

    Update CreateSubscriptionDto.cs

--- Sufra.Application/DTOs/Subscriptions/CreateSubscriptionDto.cs
+++ Sufra.Application/DTOs/Subscriptions/CreateSubscriptionDto.cs
@@ -1,8 +1,8 @@
-namespace Sufra.Application.DTOs.Subscriptions
-{
-    public class CreateSubscriptionDto
-    {
-        public int StudentId { get; set; }
-        public string PlanCode { get; set; } = "monthly_30";
-    }
-}
+// namespace Sufra.Application.DTOs.Subscriptions
+// {
+//     public class CreateSubscriptionDto
+//     {
+//         public int StudentId { get; set; }
+//         public string PlanCode { get; set; } = "monthly_30";
+//     }
+// }
